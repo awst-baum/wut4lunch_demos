f56a5859fcd62c91340e45296eb5c04cb45082ab
D. Baum <baum@awst.at>
2018-07-31 17:19:31 +0200

    Adding REST interface to Flask example (no PUTting though)

--- flask_wut4lunch/wut4lunch.py
+++ flask_wut4lunch/wut4lunch.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_restful import fields, reqparse, marshal_with, abort
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+rest_api = Api(app)
 
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
@@ -11,6 +15,9 @@ class Lunch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     submitter = db.Column(db.String(63))
     food = db.Column(db.String(255))
+
+    def __str__(self):
+        return "{} ate {}".format(self.submitter, self.food)
 
 #from flask_wtf import Form
 from flask_wtf import FlaskForm as Form
@@ -43,6 +50,43 @@ def newlunch():
         db.session.commit()
     return redirect(url_for('root'))
 
+
+lunches = Lunch.query.all()
+
+resource_fields = {
+    'submitter':   fields.String,
+    'food':        fields.String,
+#     'uri':         fields.Url('lunch_ep')
+}
+
+parser = reqparse.RequestParser()
+parser.add_argument('submitter')
+parser.add_argument('food')
+
+class RestLunch(Resource):
+    @marshal_with(resource_fields)
+    def get(self, lunch_id):
+        try:
+            l = lunches[lunch_id]
+        except IndexError as ie:
+            abort(404, message="Lunch {} doesn't exist".format(lunch_id))
+            
+        return l
+
+    @marshal_with(resource_fields)
+    def put(self, lunch_id):
+        newlunch = Lunch() ## TODO: figure out how to get data passed
+        try:
+            lunches[lunch_id] = newlunch
+            db.session.add(newlunch)
+            db.session.commit()
+        except IndexError as ie:
+            abort(404, message="Lunch {} doesn't exist".format(lunch_id))
+            
+        return newlunch, 201
+
+
+rest_api.add_resource(RestLunch, '/rest/<int:lunch_id>', endpoint='lunch_ep')
 
 if __name__ == "__main__":
     db.create_all()  # make our sqlalchemy tables
